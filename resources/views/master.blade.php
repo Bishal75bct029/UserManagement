458b75c148238d6aadd90538986af4e54d15fcd7
Bishal Lamichhane <bishallc175@gmail.com>
2023-06-19 15:40:05 +0545

    Simple View Created

---
+++ resources/views/master.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title','CRUD APP')</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <!-- @vite('resources/css/app.css') -->
+
+</head>
+<body>
+    @yield('content','HI')
+</body>
+</html>
